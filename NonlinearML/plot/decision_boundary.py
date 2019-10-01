Implemented linear+rank model. Fixed Tensorflow model with correct softmax layer.

NonlinearML/plot/decision_boundary.py
@@ -34,11 +34,11 @@ def setup_axes(fig, rect, rotation, axisScale, axisLimits, doShift):
     return ax, aux_ax
 
 def decision_boundary(
-    model, df, features, label_reg=False,
+    model, df, features, rank=False,
     h=0.01, x_label="", y_label="", xlim=False, ylim=False,
     title=False, title_loc='center', annot=False, vlines=[], hlines=[],
-    scatter=False, subsample=0.01, label_cla=None, scatter_legend=False,
-    dist=False, nbins=20,
+    scatter=False, subsample=0.01, scatter_legend=False,
+    label_cla=None, dist=False, nbins=20,
     colors=["#BA2832", "#F3F2F2", "#2A71B2"], figsize=(8,4), ticks=[],
     filename=""):
     ''' Plot decision boundary of trained model.
@@ -60,9 +60,12 @@ def decision_boundary(
         subsample: subsampling rate used for scatter plot.
             ex. subsampling=0.1 means only 10% of data will be plotted.
             Used for scatter plot.
+            
+            If a dataframe is given, scatter plot is created using all samples
+            in the dataframe.
         dist: Plot distribution of two features.
         n_bins: Number of bins in histogram.
-        label_reg: If specified, model is triained on this regression label.
+        rank: If True, prediction is made by ranking the regression output.
         others: plotting option
     Returns:
         None
@@ -85,7 +88,7 @@ def decision_boundary(
     df_mesh = pd.DataFrame(np.c_[xx.ravel(), yy.ravel()])\
                 .rename({i:features[i] for i in range(len(features))}, axis=1)
     # Make prediction for each point on grid
-    if label_reg:
+    if rank:
         z = model.predict(df_mesh, None)
     else:
         z = model.predict(df_mesh)
@@ -113,11 +116,14 @@ def decision_boundary(
     im = ax.pcolormesh(xx, yy, z, cmap=cmap)
     # Add scatter plot
     if scatter:
-        df_sub = df.sample(frac=subsample)
+        if type(subsample)==float:
+            df_sub = df.sample(frac=subsample)
+        elif type(subsample)==pd.DataFrame:
+            df_sub = subsample
         for i, cls in enumerate(sorted(df_sub[label_cla].unique())):
             _df = df_sub.loc[df_sub[label_cla]==cls]
             ax.scatter(
-                x=_df[features[0]], y=_df[features[1]], s=25,
+                x=_df[features[0]], y=_df[features[1]], s=50,
                 edgecolors='black', c=colors[i], label=cls)
             if scatter_legend==True:
                 ax.legend(loc='lower right')
@@ -157,15 +163,14 @@ def decision_boundary(
 
 
 
-def decision_boundary_multiple_hparmas(param_grid, label, db_annot_x, db_annot_y, label_reg=False, **kwargs):
+def decision_boundary_multiple_hparmas(param_grid, label, db_annot_x, db_annot_y, rank=False, **kwargs):
     ''' Plot decision boundary for each hyperparameter set. This is a wrapper
         of 'decision_boundary' function.
     Args:
         param_grid: Hyperparamater grid to search.
         label: classification label
         db_annot_x, db_annot_y: Location of annotation that displays parameters.
-        label_reg: If specified, model is triained on this regression label.
-                   Label is ignored.
+        rank: If True, prediction is made by ranking the regression output.
         **kwargs:
             Arguments for 'decision_boundary'. Only difference is
             that df must be training data.
@@ -193,10 +198,7 @@ def decision_boundary_multiple_hparmas(param_grid, label, db_annot_x, db_annot_y
         io.message(["\t - "+x+"="+str(params[x]) for x in params])
         # Train model with given hyperparameter set
         model=model.set_params(**params)
-        if label_reg:
-            model.fit(df_train[features], df_train[label_reg])
-        else:
-            model.fit(df_train[features], df_train[label])
+        model.fit(df_train[features], df_train[label])
         # Assign trained model back to kwargs
         kwargs['model'] = model
         # Add counter to filename
@@ -207,7 +209,7 @@ def decision_boundary_multiple_hparmas(param_grid, label, db_annot_x, db_annot_y
                 'text':get_param_string(params).strip('{}')\
                     .replace('\'','').replace(',','\n').replace('\n ', '\n'),
                 'x':db_annot_x, 'y':db_annot_y},
-            label_reg=label_reg,
+            rank=rank,
             **kwargs)
     return
 

NonlinearML/tf/model.py
@@ -36,14 +36,14 @@ class TensorflowModel:
             str(datetime.now()).replace(' ', '_')]).strip('\'')
         return tf.keras.callbacks.TensorBoard(
             log_dir=path,
-            histogram_freq=True, update_freq='epoch')
+            histogram_freq=False, update_freq='epoch')
 
     def get_earlystop(self):
         """ Add early stopping and tfboard to callback"""
         return tf.keras.callbacks.EarlyStopping(
             monitor=self.params['metrics'].name,
             patience=self.params["patience"],
-            mode='max')
+            mode='auto')
 
     def set_params(self, **params):
         """ Set model parameters. """
@@ -73,18 +73,19 @@ class TensorflowModel:
                 batch_size=self.params['batch_size'],
                 validation_split=self.params["validation_split"],
                 callbacks=[self.get_earlystop(), self.get_tfboard()],
-                verbose=0)
+                verbose=1)
         else:
             print("Set hyperparameters first by .set_params method")
         return self
 
     def predict(self, x):
         """ Make prediction."""
-        y_prob = self.model.predict(x, batch_size=self.params['batch_size'])
+        #y_prob = self.model.predict(x.values, batch_size=self.params['batch_size'])
+        y_prob = self.model.predict_on_batch(x.values)
+        if 'numpy' in dir(y_prob):
+            y_prob = y_prob.numpy() # Convert to numpy if trained on GPU
         y_classes = y_prob.argmax(axis=-1)
         return y_classes
-        #return self.model.predict_classes(
-        #    x=x, verbose=0, batch_size=self.params['batch_size'])
 
 
 def extract_metrics(class_report, num_class):

run/asset_growth_cla.py
@@ -10,6 +10,7 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 import tensorflow as tf
 import warnings
 from xgboost.sklearn import XGBClassifier
@@ -29,6 +30,7 @@ import NonlinearML.tf.model as tfmodel
 import NonlinearML.model.linearRank as linearRank
 import NonlinearML.interprete.classification as classification
 
+
 # Supress warnings
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -39,114 +41,90 @@ pd.options.mode.chained_assignment = None
 # Set configuration
 #-------------------------------------------------------------------------------
 # Set input and output path
-#INPUT_PATH = '/mnt/mainblob/nonlinearML/EnhancedDividend/data/Data_EM_extended.csv'
-INPUT_PATH = '../EnhancedDividend/data/Data_EM_extended.csv'
+INPUT_PATH = '../data/\
+Data_for_AssetGrowth_Context.r5.p2.csv'
 
 # Set features of interest
-feature_x = 'DividendYield'
-feature_y = 'Payout_E'
-# Set limits of decision boundary
-db_xlim = (0, 0.2)
-db_ylim = (-1, 1.5)
-db_res = 0.0005
-
-
-# Set features of interest
-#feature_x = 'DY_dmed'
-#feature_y = 'PO_dmed'
-# Set limits of decision boundary
-#db_xlim = (-3, 3)
-#db_ylim = (-3, 3)
-#db_res = 0.01
-
-
-# Set features of interest
-#feature_x = 'DividendYield'
-#feature_y = 'EG'
-# Set limits of decision boundary
-#db_xlim = (0, 0.2)
-#db_ylim = (-0.5, 0.5)
-#db_res = 0.0005
-
-
-# Set features of interest
-#feature_x = 'DY_dmed'
-#feature_y = 'EG_dmed'
-# Set limits of decision boundary
-#db_xlim = (-3, 3)
-#db_ylim = (-3, 3)
-#db_res = 0.01
-
-
-
-
+""" Available features:
+'GICSSubIndustryNumber', 'CAP', 'AG', 'ROA', 'ES', 'LTG', 'SG', 'CVROIC',
+'GS', 'SEV', 'FCFA', 'ROIC', 'Momentum' """
+feature_x = 'AG'
+feature_y = 'FCFA'
 
 # Set path to save output figures
 output_path = 'output/%s_%s/' % (feature_x, feature_y)
 tfboard_path='tf_log/%s_%s/' % (feature_x, feature_y)
 
 # Set labels
-n_classes=10
-class_label={x+1:'D'+str(x+1) for x in range(10)}
-class_order = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] # High return to low return
+n_classes=3
+class_label={0:'T1', 1:'T2', 2:'T3'}
+class_order = [0, 1, 2] # High return to low return
+#n_classes=4
+#class_label={0:'Q1', 1:'Q2', 2:'Q3', 3:'Q4'}
+#class_order = [0, 1, 2, 3] # High return to low return
+suffix="descrite"
 
 # Set output label classes
-label_reg = 'fqRet' # continuous target label
-label_cla = 'QntfqRet' # discretized target label
-label_fm = 'fmRet' # monthly return used for calculating cum. return
+label_reg = "fqTotalReturn" # continuous target label
+label_cla = "_".join([label_reg, suffix]) # discretized target label
+label_fm = "fmTotalReturn" # monthly return used for calculating cum. return
 
 # Set data column
-date_column = "smDate"
+date_column = "eom"
 
 # Set security ID column
 security_id = 'SecurityID'
 
 # Set train and test period
-test_begin = "2012-01-01"
-test_end = "2019-05-01"
+test_begin = "2011-01-01"
+test_end = "2017-11-01"
 
 # Set cross-validation configuration
-k = 5             # Must be > 1
+k =  2         # Must be > 1
 n_epoch = 1
-subsample = 0.5
+subsample = 0.8
 purge_length = 3
 
-# Set p-value threshold for ANOVA test p_thres = 0.05
+# Set p-value threshold for ANOVA test
 p_thres = 0.05
+
+# Set limits of decision boundary
+db_xlim = (-3, 3)
+db_ylim = (-3, 3)
+db_res = 0.01
+db_figsize= (10,10)
+db_annot_x=0.02
+db_annot_y=0.98
+db_nbins=50
 
 # Set metric for training
 cv_metric = ['f1-score', 'precision', 'recall', 'accuracy']
 
 # Set color scheme for decision boundary plot
-cmap = matplotlib.cm.get_cmap('Spectral', 10)
-db_colors = [matplotlib.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
-
-# Set decision boundary plotting options
-db_figsize= (8, 8)
-db_annot_x=0.02
-db_annot_y=0.98
-db_nbins=50
+db_colors = ["#3DC66D", "#F3F2F2", "#DF4A3A"]
+#cmap = matplotlib.cm.get_cmap('RdYlGn', 3)
+#db_colors = [matplotlib.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
 
 # Set algorithms to run
 run_lr = False
-run_lr_rank = False
+run_lr_rank = True
 run_xgb = False
-run_knn = False
 run_svm = False
+run_knn = False
 run_nn = False
 run_comparison = False
 save_prediction = False
 
-# Set default warning
-warnings.filterwarnings('once')  # 'error', 'always', 'ignore'
+
 
 #-------------------------------------------------------------------------------
 # Convert configuration as a dictionary
 #-------------------------------------------------------------------------------
 config = {
     'feature_x':feature_x, 'feature_y':feature_y,
-    'output_path':output_path, 'security_id':security_id,
+    'output_path':output_path,
     'n_classes':n_classes, 'class_label':class_label, 'class_order':class_order,
+    'suffix':suffix, 'security_id':security_id,
     'label_reg':label_reg, 'label_cla':label_cla, 'label_fm':label_fm,
     'date_column':date_column, 'test_begin':test_begin, 'test_end':test_end,
     'k':k, 'n_epoch':n_epoch, 'subsample':subsample,
@@ -161,12 +139,17 @@ config = {
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
 
-
     #---------------------------------------------------------------------------
     # Read dataset
     #---------------------------------------------------------------------------
     # Read input csv
     df = pd.read_csv(INPUT_PATH, index_col=None, parse_dates=[date_column])
+
+    # Discretize target label
+    df = utils.discretize_variables_by_month(
+        df=df, variables=[label_reg], month=date_column, n_classes=n_classes,
+       class_names=sorted(np.arange(n_classes), reverse=True),
+        suffix=suffix)
 
     # Split dataset into train and test dataset
     df_train, df_test = cv.train_test_split_by_date(
@@ -181,11 +164,12 @@ if __name__ == "__main__":
             "penalty":['l2'],
             "multi_class":['multinomial'],
             "solver":['newton-cg'],
-            "max_iter":[50],
+            #"max_iter":[100],
+            "max_iter":[10],
             "tol": [1e-2],
             "n_jobs":[-1],
-            "C": [1] + np.logspace(-4, 4, 10)} # C <= 1e-5 doesn't converge
-            #"C": [1]}
+            "C": [1]} # note: C <= 1e-5 doesn't converge
+            #"C": [1] + np.logspace(-4, 4, 11)} # note: C <= 1e-5 doesn't converge
 
         # Set model
         model_lr = LogisticRegression()
@@ -195,11 +179,8 @@ if __name__ == "__main__":
         db_lr = classification.decision_boundary2D(
             config, df_train, df_test,
             model_lr, model_lr_str, param_grid_lr, best_params={},
-            read_last=False,
-            cv_study=True,
-            run_backtest=True,
-            plot_decision_boundary=True,
-            save_csv=True,
+            read_last=False, cv_study=True, run_backtest=True,
+            plot_decision_boundary=True, save_csv=True,
             return_train_ylim=(-1,20), return_test_ylim=(-1,5))
 
     #---------------------------------------------------------------------------
@@ -208,7 +189,8 @@ if __name__ == "__main__":
     if run_lr_rank:
         # Set parameters to search
         param_grid_lr = {
-            "alpha": [1] + np.logspace(-4, 4, 10)} # note: C <= 1e-5 doesn't converge
+            "alpha": [1]}
+            #"alpha": [1] + np.logspace(-4, 4, 11)} # note: C <= 1e-5 doesn't converge
 
         # Set model
         model_lr_rank = linearRank.LinearRank(
@@ -217,13 +199,13 @@ if __name__ == "__main__":
         model_lr_rank_str = 'lr_rank'
 
         # Run analysis on 2D decision boundary
-        db_lr = classification.decision_boundary2D(
+        db_lr_rank = classification.decision_boundary2D(
             config, df_train, df_test,
             model_lr_rank, model_lr_rank_str, param_grid_lr, best_params={},
             read_last=False, cv_study=True, run_backtest=True,
             plot_decision_boundary=True, save_csv=True,
             return_train_ylim=(-1,20), return_test_ylim=(-1,5),
-            label_reg=config['label_reg'])
+            rank=True)
 
     #---------------------------------------------------------------------------
     # Xgboost
@@ -231,57 +213,26 @@ if __name__ == "__main__":
     if run_xgb:
         # Set parameters to search
         param_grid_xgb = {
-            'min_child_weight': [1000], #[1000, 500], #[1000], 
-            'max_depth': [5, 10],
-            'eta': [0.3, 0.01, 0.1, 0.5], #[0.3]
-            'n_estimators': [50, 100], # [50, 100, 200],
+            'gamma': [10], #np.logspace(-2, 1, 1), # Min loss reduction
+            'min_child_weight': [1500, 1000, 500],
+            'max_depth': [5, 7, 10],
+            'learning_rate': [0.3],
+            'n_estimators': [50],
             'objective': ['multi:softmax'],
-            'gamma': [0], #[0, 5, 10],
-            'lambda': [1], #np.logspace(0, 2, 3), #[1], # L2 regularization
+            'lambda': [1], #np.logspace(0, 2, 2) # L2 regularization
             'n_jobs':[-1],
-            'subsample': [1],#[1, 0.8, 0.5], # [1]
-            'num_class': [n_classes]}
-
-
+            'num_class': [3]}
 
         # Set model
         model_xgb = XGBClassifier()
-        model_xgb_str = 'xgb_eta'
+        model_xgb_str = 'xgb'
 
         # Run analysis on 2D decision boundary
         db_xgb = classification.decision_boundary2D(
             config, df_train, df_test,
             model_xgb, model_xgb_str, param_grid_xgb, best_params={},
-            read_last=False,
-            cv_study=True,
-            run_backtest=True,
-            plot_decision_boundary=True,
-            save_csv=True,
-            return_train_ylim=(-1,20), return_test_ylim=(-1,5))
-
-
-    #---------------------------------------------------------------------------
-    # kNN
-    #---------------------------------------------------------------------------
-    if run_knn:
-        # Set parameters to search
-        param_grid_knn = {
-            'n_neighbors':
-                sorted([int(x) for x in np.logspace(2, 3, 10)], reverse=True)}
-
-        # Set model
-        model_knn = KNeighborsClassifier()
-        model_knn_str = 'knn'
-
-        # Run analysis on 2D decision boundary
-        db_knn = classification.decision_boundary2D(
-            config, df_train, df_test,
-            model_knn, model_knn_str, param_grid_knn, best_params={},
-            read_last=False,
-            cv_study=True,
-            run_backtest=True,
-            plot_decision_boundary=True,
-            save_csv=True,
+            read_last=False, cv_study=True, run_backtest=True,
+            plot_decision_boundary=True, save_csv=True,
             return_train_ylim=(-1,20), return_test_ylim=(-1,5))
 
 
@@ -306,11 +257,29 @@ if __name__ == "__main__":
         db_svm = classification.decision_boundary2D(
             config, df_train, df_test,
             model_svm, model_svm_str, param_grid_svm, best_params={},
-            read_last=False,
-            cv_study=True,
-            run_backtest=True,
-            plot_decision_boundary=True,
-            save_csv=True,
+            read_last=False, cv_study=True, run_backtest=True,
+            plot_decision_boundary=True, save_csv=True,
+            return_train_ylim=(-1,20), return_test_ylim=(-1,5))
+
+    #---------------------------------------------------------------------------
+    # kNN
+    #---------------------------------------------------------------------------
+    if run_knn:
+        # Set parameters to search
+        param_grid_knn = {
+            'n_neighbors':
+                sorted([int(x) for x in np.logspace(2, 3, 10)], reverse=True)}
+
+        # Set model
+        model_knn = KNeighborsClassifier()
+        model_knn_str = 'knn'
+
+        # Run analysis on 2D decision boundary
+        db_knn = classification.decision_boundary2D(
+            config, df_train, df_test,
+            model_knn, model_knn_str, param_grid_knn, best_params={},
+            read_last=True, cv_study=False, run_backtest=False,
+            plot_decision_boundary=True, save_csv=False,
             return_train_ylim=(-1,20), return_test_ylim=(-1,5))
 
 
@@ -318,56 +287,51 @@ if __name__ == "__main__":
     # Neural net
     #---------------------------------------------------------------------------
     if run_nn:
-        # Define ensemble of weak leaners
-        ensemble = []
-        input_layer = tf.keras.Input(shape=(2,))
-        for i in range(100):
-            weak_learner = tf.keras.layers.Dense(units=128, activation='relu',
-                kernel_initializer=tf.initializers.GlorotUniform())(input_layer)
-            weak_learner = tf.keras.layers.Dropout(0.5)(weak_learner)
-            weak_learner = tf.keras.layers.Dense(units=128, activation='relu',
-                kernel_initializer=tf.initializers.GlorotUniform())(weak_learner)
-            weak_learner = tf.keras.layers.Dropout(0.5)(weak_learner)
-            ensemble.append(tf.keras.layers.Dense(units=128, activation='relu',
-                kernel_initializer=tf.initializers.GlorotUniform())(weak_learner))
-        output_layer = tf.keras.layers.Average()(ensemble)
-        ensemble_model = tf.keras.Model(input_layer, output_layer)
 
-        param_grid = {
-            'learning_rate': [5e-4], #np.logspace(-4,-2,6),
-            'metrics': [
-                tf.keras.metrics.SparseCategoricalCrossentropy()],
-                #tf.keras.metrics.SparseCategoricalAccuracy()],
-            'patience': [3, 5, 20], # 3,4,5
-            #'patience': [5], # 3,4,5
-            'epochs': [1000],
-            'validation_split': [0.2],
-            'batch_size': [32],
-            'model': [
-                #tf.keras.Sequential([
-                #    tf.keras.layers.Dense(128, activation='relu'),
-                #    tf.keras.layers.Dropout(0.5),
-                #    tf.keras.layers.Dense(128, activation='relu'),
-                #    tf.keras.layers.Dropout(0.5),
-                #    tf.keras.layers.Dense(128, activation='relu'),
-                #    tf.keras.layers.Dense(n_classes, activation='softmax')]),
-                ensemble_model
-                ]
-            }
+        strategy = tf.distribute.MirroredStrategy()
+        with strategy.scope():
+            # Define ensemble of weak leaners
+            ensemble = []
+            input_layer = tf.keras.Input(shape=(2,))
+            for i in range(100):
+                weak_learner = tf.keras.layers.Dense(units=32, activation='relu',
+                    kernel_initializer=tf.initializers.GlorotUniform())(input_layer)
+                weak_learner = tf.keras.layers.Dropout(0.5)(weak_learner)
+                weak_learner = tf.keras.layers.Dense(units=32, activation='relu',
+                    kernel_initializer=tf.initializers.GlorotUniform())(weak_learner)
+                weak_learner = tf.keras.layers.Dropout(0.5)(weak_learner)
+                ensemble.append(tf.keras.layers.Dense(units=3, activation='softmax',
+                    kernel_initializer=tf.initializers.GlorotUniform())(weak_learner))
+            output_layer = tf.keras.layers.Average()(ensemble)
+            ensemble_model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
 
-        # Build model and evaluate
-        model = tfmodel.TensorflowModel(
-            model=None, params={}, log_path=tfboard_path)
-        model_str = 'nn'
+            param_grid = {
+                'learning_rate': [5e-4], #np.logspace(-4,-2,6),
+                'metrics': [
+                    tf.keras.metrics.SparseCategoricalCrossentropy()],
+                    #tf.keras.metrics.SparseCategoricalAccuracy()],
+                #'patience': [5, 50, 100], # 3,4,5
+                'patience': [5], # 3,4,5
+                'epochs': [1000],
+                'validation_split': [0.2],
+                'batch_size': [32],
+                'model': [
+                    ensemble_model
+                    ]
+                }
 
-        # Run analysis on 2D decision boundary
-        db = classification.decision_boundary2D(
-            config, df_train, df_test,
-            model, model_str, param_grid, best_params={},
-            read_last=False, cv_study=True, run_backtest=True,
-            plot_decision_boundary=True, save_csv=True,
-            return_train_ylim=(-1,20), return_test_ylim=(-1,5))
+            # Build model and evaluate
+            model = tfmodel.TensorflowModel(
+                model=None, params={}, log_path=tfboard_path)
+            model_str = 'nn'
 
+            # Run analysis on 2D decision boundary
+            db = classification.decision_boundary2D(
+                config, df_train, df_test,
+                model, model_str, param_grid, best_params={},
+                read_last=False, cv_study=False, run_backtest=True,
+                plot_decision_boundary=True, save_csv=True,
+                return_train_ylim=(-1,20), return_test_ylim=(-1,5))
 
 
 
@@ -375,13 +339,14 @@ if __name__ == "__main__":
     # Compare model results
     #---------------------------------------------------------------------------
     if run_comparison:
-        summary = summary.model_comparison(
-            models=['lr', 'lr_rank', 'knn', 'xgb'],
-            output_path=output_path,
-            label_reg=config['label_reg'],
+        model_comparison = summary.model_comparison(
+            #models=['lr', 'knn'], output_path=output_path,
+            #models=['xgb', 'lr', 'lr_rank', 'knn', 'nn'], output_path=output_path,
+            models=['nn_best', 'xgb', 'lr_rank', 'knn', 'lr' ], output_path=output_path,
+            label_reg=config['label_fm'],
             class_label=config['class_order'],
             date_column=config['date_column'],
-            ylim=(-0.25,1))
+            ylim=(0,1.5))
 
 
     #---------------------------------------------------------------------------
@@ -389,7 +354,7 @@ if __name__ == "__main__":
     #---------------------------------------------------------------------------
     if save_prediction:
         predictions = summary.save_prediction(
-            models=['lr', 'lr_rank', 'knn', 'xgb'],
+            models=['lr', 'lr_rank', 'knn', 'xgb', 'nn_best'],
             feature_x=config['feature_x'],
             feature_y=config['feature_y'],
             df_input=df,
@@ -399,6 +364,20 @@ if __name__ == "__main__":
 
 
 
+        for model in ['lr', 'lr_rank', 'knn', 'xgb', 'nn_best']:
+            predictions['pred_train'][model]
+            print(model)
+            _sum = predictions['pred_train'][model]['pred'].value_counts().sum()
+            top = predictions['pred_train'][model]['pred'].value_counts()[0.0]
+            mid = predictions['pred_train'][model]['pred'].value_counts()[1.0]
+            bot = predictions['pred_train'][model]['pred'].value_counts()[2.0]
+            print(str(top)+","+str(mid)+","+str(bot))
+            print(_sum)
+            print('Top %% = %s' % str(top / _sum))
+            print('Mid %% = %s' % str(mid / _sum))
+            print('Bottom %% = %s' % str(bot / _sum))
+
+    
 
 
 