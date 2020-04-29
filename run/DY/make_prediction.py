aa840cf2c48a1a5f2247849fe4afd2ca4d33250c
Siinn Che <siinn.che@gmail.com>
2020-04-29 19:27:50 +0000

    Example of making predictions

--- run/DY/make_prediction.py
+++ run/DY/make_prediction.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # Import common python libraries
-#from datetime import datetime
-#import dateutil.relativedelta
 import matplotlib
 #import numpy as np
 #import os
@@ -11,12 +9,10 @@ import sys
 import warnings
 
 # Import custom libraries
-#import NonlinearML.lib.stats as stats
-#import NonlinearML.lib.summary as summary
-import NonlinearML.lib.utils as utils
 import NonlinearML.lib.io as io
 import NonlinearML.plot.plot as plot
 import NonlinearML.plot.decision_boundary as plot_db
+from xgboost.sklearn import XGBRegressor
 
 
 # Supress warnings
@@ -28,13 +24,10 @@ pd.options.mode.chained_assignment = None
 # Set configuration
 #-------------------------------------------------------------------------------
 # Set input and output path
-INPUT_PATH = '/mnt/mainblob/nonlinearML/EnhancedDividend/data/production/DYPO_2020.01.30.p1.csv'
+INPUT_PATH = '/mnt/mainblob/nonlinearML/EnhancedDividend/data/production/Production.20200305.p1.csv'
 
 # Set features of interest
-#features = ['DividendYield', 'Payout_E']
 features = ['DY_dmed', 'PO_dmed']
-#features = ['DividendYield', 'EG']
-#features = ['DY_dmed', 'EG_dmed']
 
 # Set number of bins for ranking
 rank_n_bins=10
@@ -43,15 +36,10 @@ rank_order=[9,8,7,6,5,4,3,2,1,0] # High return to low return
 rank_top = 9
 rank_bottom = 0
 
-# Set output label classes
-#label_reg = 'fqRet' # continuous target label
-#label_cla = 'QntfqRet' # discretized target label
-#label_cla = 'fqRet_discrete' # discretized target label
-#label_fm = 'fmRet' # monthly return used for calculating cum. return
-
 # Set path to save output figures
+model_tag = 'v1'
 output_path = 'output/DY/Production/%s/' % "_".join(features)
-MODEL_PATH = 'model/DY/DY_dmed_PO_dmed/v2/xgb.pickle'
+MODEL_PATH = '/mnt/mainblob/nonlinearML/NonlinearML/model/DY/DY_dmed_PO_dmed/%s/enhanced_DY_xgboost.%s' %(model_tag, model_tag)
 
 # Set data column
 date_column = "smDate"
@@ -69,7 +57,7 @@ db_colors_scatter = [matplotlib.colors.rgb2hex(cmap_scatter(i))
     for i in range(cmap_scatter.N)]
 
 # Set decision boundary plotting options
-db_xlim = (-3, 3)
+db_xlim = (-1.5, 3)
 db_ylim = (-3, 3)
 db_res = 0.01
 db_figsize= (10, 8)
@@ -92,13 +80,13 @@ if __name__ == "__main__":
     # Read input csv
     io.title("Reading input file")
     io.message("\t> Input = %s" % INPUT_PATH)
-    df = pd.read_csv(INPUT_PATH, index_col=None, parse_dates=[date_column])
+    df = pd.read_csv(INPUT_PATH, index_col=None)
 
     # Load model
     io.title("Loading trained model")
     io.message("\t> Model path = %s" % MODEL_PATH)
-    with open(MODEL_PATH, 'rb') as f:
-        model = pickle.load(f)
+    model = XGBRegressor()
+    model.load_model(MODEL_PATH)
 
     # Print model parameters
     if 'get_params' in dir(model):
@@ -107,20 +95,20 @@ if __name__ == "__main__":
             for key in model.get_params()])
     io.message("Features: %s" %features)
 
-    # Check if feature names matches
-    if not features==model.get_booster().feature_names:
-        io.message("Features don't match. Terminating job..")
-        sys.exit("Features don't match. Terminating job..")
-
     # Make prediction
     io.title("Making predictions")
-    df['pred'] = model.predict(df[features])
+    df['pred'] = model.predict(df[features], validate_features=False)
+
 
     #---------------------------------------------------------------------------
-    # Examining output
+    # Prediction distribution
     #---------------------------------------------------------------------------
     # Plot distribution of prediction
     io.message("Plotting prediction distribution")
+    df_MSCIEM = df.loc[df['IsMSCIEM']==1]
+    df_nonMSCIEM = df.loc[df['IsMSCIEM']==0]
+
+    # All data
     plot.plot_distribution(
         df=df, columns=['pred'], n_rows=1, n_columns=1,
         bins=[50], ylog=[False], title=[""],
@@ -128,6 +116,25 @@ if __name__ == "__main__":
         figsize=(8,6), filename=output_path+"dist_pred",
         show_sigma=False, vlines=None)
 
+    # MSCIEM==1
+    plot.plot_distribution(
+        df=df_MSCIEM, columns=['pred'], n_rows=1, n_columns=1,
+        bins=[50], ylog=[False], title=[""],
+        x_label=['Prediction'], y_label=['Samples'],
+        figsize=(8,6), filename=output_path+"dist_pred_MSCIEM",
+        show_sigma=False, vlines=None)
+
+    # MSCIEM==0
+    plot.plot_distribution(
+        df=df_nonMSCIEM, columns=['pred'], n_rows=1, n_columns=1,
+        bins=[50], ylog=[False], title=[""],
+        x_label=['Prediction'], y_label=['Samples'],
+        figsize=(8,6), filename=output_path+"dist_pred_nonMSCIEM",
+        show_sigma=False, vlines=None)
+
+    #---------------------------------------------------------------------------
+    # Decision boundary
+    #---------------------------------------------------------------------------
     # Plot decision boundary of the best model with scattered plot.
     io.message("Plotting decision boundaries")
     plot_db.decision_boundary(
@@ -138,20 +145,15 @@ if __name__ == "__main__":
         vmin=db_vmin, vmax=db_vmax,
         figsize=db_figsize,
         colorbar=True, ticks=None,
-        #annot={
-        #    'text':utils.get_param_string(model.get_params()).strip('{}')\
-        #        .replace('\'','').replace(',','\n').replace('\n ', '\n'),
-        #    'x':db_annot_x, 'y':db_annot_y},
         scatter=True, subsample=0.01, label_cla=None,
         scatter_legend=False, colors_scatter=db_colors_scatter,
         dist=True, nbins=db_nbins,
         filename=output_path+"decision_boundary")
 
+    #---------------------------------------------------------------------------
     # Save predictions as csv
+    #---------------------------------------------------------------------------
     df.to_csv(output_path+'pred.csv')
-
-
-
 
 
 
